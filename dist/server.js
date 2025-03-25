b0f0a6834dfcdbb79f3ea89cefc358f387aee230
Amaan30 <93734454+Amaan30@users.noreply.github.com>
2025-03-25 12:11:40 +0530

    basic update

--- dist/server.js
+++ dist/server.js
@@ -1,29 +1,11 @@
 "use strict";
-// import express, { Request, Response } from 'express';
-// import dotenv from 'dotenv';
-// import cors from "cors";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// dotenv.config();
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-// app.use(express.json());
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173", "https://your-netlify-app.netlify.app"],
-//     credentials: true,
-//   })
-// );
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello from Butterfly Server!');
-// });
-// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
-const node_path_1 = __importDefault(require("node:path"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const dotenv_1 = __importDefault(require("dotenv"));
@@ -54,12 +36,12 @@ app.use('/api/users', userRoutes_1.default);
 app.get('/', (_req, res) => {
     res.send('Welcome to the server!');
 });
-if (process.env.NODE_ENV === 'production') {
-    app.use(express_1.default.static(node_path_1.default.join(process.cwd(), 'dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(node_path_1.default.join(process.cwd(), 'dist', 'index.html'));
-    });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(process.cwd(), 'dist')));
+//   app.get('*', (req: Request, res: Response) => {
+//     res.sendFile(path.join(process.cwd(), 'dist', 'index.html'));
+//   });
+// }
 app.get('/api/data', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
 });
