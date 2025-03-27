7129db3a72c413eee817b20db63529951a178262
Amaan30 <93734454+Amaan30@users.noreply.github.com>
2025-03-28 00:30:08 +0530

    basic update

--- src/model/user.ts
+++ src/model/user.ts
@@ -17,7 +17,7 @@ const userSchema: Schema<IUser> = new Schema({
         email: {type: String, required: true, unique: true},
         username: {type: String, required: true, unique: true},
         password: {type: String, required: true},
-        profilePicture: {type: String, default: "../../uploads/profilePics/Default-pfp.jpg"},
+        profilePicture: {type: String, default: "/images/Default-pfp.jpg"},
         bio: {type: String},
         followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // ✅ Reference to other users
         following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // ✅ Reference to other users
