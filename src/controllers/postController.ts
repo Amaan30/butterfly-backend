44368f0e60c23f5c7f0aa0f8d4be603589e986ce
Amaan30 <93734454+Amaan30@users.noreply.github.com>
2025-04-09 22:49:36 +0530

    basic update

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -40,3 +40,18 @@ export const createPost = async (req: AuthRequest, res: Response) => {
     return;
   }
 };
+
+export const getUserPosts = async (req: AuthRequest, res: Response) => {
+  try {
+    const username = req.params;
+    const user = await User.findOne({ username }).populate('posts');
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+      return;
+    }
+    res.status(200).json(user.posts);
+  } catch (err) {
+    console.error('Error fetching user posts:', err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { upload } from '../utils/uploads';
-import { createPost } from '../controllers/postController';
+import { createPost, getUserPosts } from '../controllers/postController';
 import { authMiddleware } from '../middleware/auth';
+import { get } from 'node:http';
 
 const router = express.Router();
 
 router.post('/', authMiddleware, upload.single('media'), createPost);
+
+router.get('/:username', authMiddleware, getUserPosts);
 
 export default router;
